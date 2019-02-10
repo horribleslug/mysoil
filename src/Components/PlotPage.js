214277d06b571ab83453bf0484b9263f82c36ae8
joshflchan <josh.fl.chan@gmail.com>
2019-02-10 11:26:00 -0800

    merge

--- src/Components/PlotPage.js
+++ src/Components/PlotPage.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import { Chart } from "react-charts";
 
 class PlotPage extends Component {
-
-
   render() {
     let plant = this.props.data.plant;
+    var waterLevels = this.props.data.waterlevels;
+    var lineChart = (
+      // A react-chart hyper-responsively and continuusly fills the available
+      // space of its parent element automatically
+      <div
+        style={{
+          width: "500px",
+          height: "300px"
+        }}
+      >
+        <Chart
+          data={[
+            {
+              label: "Series 1",
+              data: [[0, waterLevels[0]], [1, waterLevels[1]],
+              [2, waterLevels[2]], [3, waterLevels[3]]]
+            }
+          ]}
+          axes={[
+            { primary: true, type: "linear", position: "bottom" },
+            { type: "linear", position: "left" }
+          ]}
+        />
+      </div>
+    );
     if (plant){
       return (
         <div>
         <h1>{plant.charAt(0).toUpperCase() + plant.slice(1)}</h1>
           <img className="plotimg" src={require("../Assets/" + plant + ".png")} alt={plant} />
+          {lineChart}
         </div>
       );
     } else {
