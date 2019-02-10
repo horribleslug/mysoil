1238c9c0e12084d023614b2e285ed463d01d6197
joshflchan <josh.fl.chan@gmail.com>
2019-02-10 08:17:35 -0800

    added graphs

--- src/Components/PlotPage.js
+++ src/Components/PlotPage.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { Chart } from "react-charts";
 
 class PlotPage extends Component {
-
-
   render() {
     let plant = this.props.data.plant;
+    var lineChart = (
+      // A react-chart hyper-responsively and continuusly fills the available
+      // space of its parent element automatically
+      <div
+        style={{
+          width: "600px",
+          height: "300px"
+        }}
+      >
+        <Chart
+          data={[
+            {
+              label: "Series 1",
+              data: [[0, 79], [1, 74], [2, 60], [3, 57], [4, 54], [5, 53]]
+            }
+          ]}
+          axes={[
+            { primary: true, type: "linear", position: "bottom" },
+            { type: "linear", position: "left" }
+          ]}
+        />
+      </div>
+    );
     if (plant){
       return (
         <div>
         <h1>{plant}</h1>
           <img className="plotimg" src={require("../Assets/" + plant + ".png")} alt={plant} />
+          {lineChart}
         </div>
       );
     } else {
