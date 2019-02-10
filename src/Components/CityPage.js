ccee0b97395f4dc77eb418b325ea8a054f6f4b9e
Ian <ianmmah@gmail.com>
2019-02-10 09:25:47 -0800

    google login

---
+++ src/Components/CityPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CityPage extends Component {
+
+  render() {
+      return (
+        <h1>CityPage</h1>
+      );
+
+    }
+  }
+
+
+export default CityPage;
