549f9d8f1a231ba16555b43ec77be4efd7de49a4
Ian <ianmmah@gmail.com>
2019-02-10 06:44:40 -0800

    was work

--- src/Components/Plot.js
+++ src/Components/Plot.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 
 class Garden extends Component {
 
+  handleToggle = () => {
+    this.props.toggle();
+  }
 
   render() {
     let params = this.props.params;
     return (
       <div className="carouselItem">
         <img className="plotimg" src={require("../Assets/" + params.plant + ".png")} alt={params.plant} />
+        <button onClick={this.handleToggle.bind(params.plant)}>yeet</button>
       </div>
     );
   }
